f1e8293878ca5d903b116fe8c63427a18c71c827
Constantin Ovseannicov <ovseannicov.constantin@gmail.com>
2023-01-21 14:47:40 +0200

    Add info about type casting in project Input and Output

--- 004 InputAndOutput/Program.cs
+++ 004 InputAndOutput/Program.cs
@@ -63,6 +63,43 @@ namespace _004_InputAndOutput
 
             Console.WriteLine($"Name: {name3} Age: {age2} Height:{height2}m Salary:{salary}$");
 
+            /*
+             In C#, there are two types of casting:
+
+             Implicit Casting (automatically) - converting a smaller type to a larger type size
+            char -> int -> long -> float -> double
+
+            Explicit Casting (manually) - converting a larger type to a smaller size type
+            double -> float -> long -> int -> char
+             */
+
+            //Implicit Casting
+            int myInt = 9;
+            double myDouble = myInt;       // Automatic casting: int to double
+
+            Console.WriteLine(myInt);      // Outputs 9
+            Console.WriteLine(myDouble);   // Outputs 9
+
+            //Explicit Casting
+            double myDouble2 = 9.78;
+            int myInt2 = (int)myDouble2;    // Manual casting: double to int
+
+            Console.WriteLine(myDouble2);   // Outputs 9.78
+            Console.WriteLine(myInt2);      // Outputs 9
+
+            //Type Conversion Methods
+
+            int myInt3 = 10;
+            double myDouble3 = 5.25;
+            bool myBool2 = true;
+
+            Console.WriteLine(Convert.ToString(myInt3));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt3));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble3));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool2));   // convert bool to string
+
+
+
 
         }
     }
